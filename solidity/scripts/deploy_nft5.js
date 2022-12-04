8982596a1c3916424c5e6a412a8bbecc93d3415e
gouravnaik <contact@gouravnaik.dev>
2022-12-04 07:45:42 +0530

    forwarder added of polygon

--- solidity/scripts/deploy_nft5.js
+++ solidity/scripts/deploy_nft5.js
@@ -16,8 +16,8 @@ async function main() {
   // We get the contract to deploy
   const Nft5 = await hre.ethers.getContractFactory('Nft5');
   const nft5 = await Nft5.deploy(
-    '0x69015912AA33720b842dCD6aC059Ed623F28d9f7',
-    'NFT5',
+    '0xf0511f123164602042ab2bCF02111fA5D3Fe97CD',
+    'NFT5 - ETHINDIA',
     'NFT5'
   );
 
